1ed90c1834657ffcc85cde033e2d6239a60ffc04
l33th <l33th@protonmail.ch>
2022-06-08 08:16:17 +0200

    initial commitO

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+        <h2>Practical ReactJS, TypeScript &amp; Bootstrap.</h2>
+    </React.Fragment>
+  );
+}
+
+export default App;
