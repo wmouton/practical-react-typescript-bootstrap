1528b4dea9fef80679f7c5ceb23363cf266f0c36
l33th <l33th@protonmail.ch>
2022-06-08 09:57:02 +0200

    ⌨   code icon 5x ⌨

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <React.Fragment>
       <div className='heading-content'>
+        <i className='fa fa-code fa-5x' />
         <h2 className='text-primary'>ReactJS &bull; TypeScript &bull; Bootstrap</h2>
         <h5 className='rtb'>A demonstration of a practical use of ReactJS, TypeScript &amp; Bootstrap.</h5>
       </div>
