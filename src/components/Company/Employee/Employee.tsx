7d568f6c7976beb87e528e1104c2a13bb52f839e
l33th <l33th@protonmail.ch>
2022-06-30 07:00:17 +0200

    chore: using state

--- src/components/Company/Employee/Employee.tsx
+++ src/components/Company/Employee/Employee.tsx
@@ -1,12 +1,30 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
 interface EProps {}
-interface EState {}
+interface EState {
+    name: string;
+    age: number;
+    title: string;
+}
 
 const Employee: FC<EProps> = () => {
+
+    const initialState = {name: 'James Samson', age: 22, title: 'Web Developer'}
+    const [employee] = useState<EState>(initialState);
     return (
         <>
-        <h3>Employee Component</h3>
+        <h3>Company Employee</h3>
+            <ul className='list-group'>
+                <li className='list-group-item'>
+                    Name: {employee.name}
+                </li>
+                <li className='list-group-item'>
+                    Age: {employee.age}
+                </li>
+                <li className='list-group-item'>
+                    Title: {employee.title}
+                </li>
+            </ul>
         </>
     )
 }
