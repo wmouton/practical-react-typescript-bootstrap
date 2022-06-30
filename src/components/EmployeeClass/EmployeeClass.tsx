ee546a9c462bb4984eb45f52c20c8ef4e97135c9
l33th <l33th@protonmail.ch>
2022-06-30 07:37:11 +0200

    chore: display employee class info

--- src/components/EmployeeClass/EmployeeClass.tsx
+++ src/components/EmployeeClass/EmployeeClass.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
-interface EProps{}
-interface EState{}
+interface EProps{
+    name: string;
+    age: number;
+    title: string;
+}
+interface EState{
+    name: string;
+    age: number;
+    title: string;
+}
 
 export class EmployeeClass extends React.Component<EProps, EState> {
     constructor(props:EProps) {
+        const employeeObj = {name: 'Jack Renal', age: 25, title: 'Web Designer'}
+
         super(props);
+        this.state = employeeObj as EState
     }
 
     render() {
         return (
             <>
             <h3>Employee Class</h3>
+                <li className='list-group-item'>
+                    Name: {this.state.name}
+                </li>
+                <li className='list-group-item'>
+                    Age: {this.state.age}
+                </li>
+                <li className='list-group-item'>
+                    Title: {this.state.title}
+                </li>
             </>
         )
     }
