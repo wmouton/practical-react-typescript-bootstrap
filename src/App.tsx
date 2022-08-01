10f190592562f411cf95753f9f89778fa17f7b18
l33th <l33th@protonmail.ch>
2022-08-01 12:00:48 +0200

    feat(state): Counter state functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Counter from "./components/Counter/Counter";
 // import Employee from './components/Company/Employee/Employee';
 // import { EmployeeClass } from "./components/EmployeeClass/EmployeeClass";
 // import Customer from './components/Company/Customer/Customer';
@@ -27,6 +28,12 @@ const App = () => {
             </div>
           </div>
           <br/>
+
+          <div className='row'>
+            <div className='col'>
+              <Counter />
+            </div>
+          </div>
 
           {/*<div className='row'>
             <div className='col'>
