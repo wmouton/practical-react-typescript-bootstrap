bb21028edddeeb0690cc9bd33867f9c308782a4f
l33th <l33th@protonmail.ch>
2022-08-01 12:02:41 +0200

    feat(state): Counter state functionality

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useState } from 'react';
+
+interface CState {
+    count: number;
+}
+
+interface CProps { }
+
+const Counter: FC<CProps> = () => {
+    const [state, setState] = useState<CState>({
+        count: 0
+    });
+
+    const inc = (): void => {
+        setState({
+            count: state.count + 1
+        });
+    }
+
+    const decr = (): void => {
+        setState({
+            count: state.count - 1
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <p className='h3 display-3'>{state.count}</p>
+                                <button className='btn btn-danger m-1' onClick={decr}>DECR</button>{' '}
+                                <button className='btn btn-success m-1' onClick={inc}>INCR</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Counter;
