5b5b75a3178f5aecd7c3125ced3dbb0ab5472f74
l33th <l33th@protonmail.ch>
2022-08-03 06:39:08 +0200

    feat: Added state to buttons

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 
-interface GreetState {}
+interface GreetState {
+    message: string;
+}
 interface GreetProps {}
 
 const Greetings:FC<GreetProps> = () => {
+    const [state, setState] = useState<GreetState>({message: 'Hello...'});
+
+    const changeMessage = (greet: string):void => setState({message: greet});
+
     return (
         <>
-            <h3>Greetings</h3>
+            <div className='container mt-3'>
+                <div className='row'>
+                    <div className='col-md-5'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h2 className='text-white'>{state.message}</h2>
+                                <button className='btn btn-success' onClick={() => changeMessage('Good Morning!')}>Good Morning</button>{' '}
+                                <button className='btn btn-warning' onClick={() => changeMessage('Good Afternoon!')}>Good Afternoon</button>{' '}
+                                <button className='btn btn-danger' onClick={() => changeMessage('Good Evening!')}>Good Evening</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
